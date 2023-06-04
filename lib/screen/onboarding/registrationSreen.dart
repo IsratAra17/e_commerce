416631b411430e3fb3d4bf9cfaf470f4bef14f8e
s.m.israt <smisratara25@gmail.com>
2023-06-04 17:24:23 +0600

    Button added and main.dart route added

--- lib/screen/onboarding/registrationSreen.dart
+++ lib/screen/onboarding/registrationSreen.dart
@@ -3,14 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+class RegistrationScreen extends StatefulWidget {
+  const RegistrationScreen({Key? key}) : super(key: key);
 
   @override
-  State<Login> createState() => _LoginState();
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
 }
 
-class _LoginState extends State<Login> {
+class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,13 +19,13 @@ class _LoginState extends State<Login> {
           ScreenBackground(context),
 
   Container(
-    padding: EdgeInsets.all(20),
+    padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
     alignment: Alignment.center,
     child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Get Started With', style: Head1TextStyle(colorGreen)),
+            Text('Registration', style: Head1TextStyle(colorGreen)),
             SizedBox(
               height: 1,
             ),
@@ -46,19 +46,28 @@ class _LoginState extends State<Login> {
                 },
               decoration: AppInputDecoration("Password"),
             ),
-
-
             SizedBox(height: 20,),
+
+            TextFormField(
+              onChanged: (Textvalue){
+              },
+              decoration: AppInputDecoration("Name"),
+            ),
+            SizedBox(height: 20,),
+
+            TextFormField(
+              onChanged: (Textvalue){
+              },
+              decoration: AppInputDecoration("Contact"),
+            ),
+            SizedBox(height: 20,),
+
             Container(child: ElevatedButton(onPressed:(){} ,
-              style: AppButtonStyle(colorOrange), child: SuccessButtonChild("Login"),
+              style: AppButtonStyle(colorOrange), child: SuccessButtonChild("Register Confirm"),
             ),)
-
-
-
 
           ],
         ),
-
   ),
 ]
 
