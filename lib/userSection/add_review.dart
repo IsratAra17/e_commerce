579bcf3c23baa96c76932cec3bbf9ab578c52c1a
s.m.israt <smisratara25@gmail.com>
2023-06-08 14:57:43 +0600

    Error Solved

---
+++ lib/userSection/add_review.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Add_Review extends StatefulWidget {
+  const Add_Review({Key? key}) : super(key: key);
+
+  @override
+  State<Add_Review> createState() => _Add_ReviewState();
+}
+
+class _Add_ReviewState extends State<Add_Review> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
